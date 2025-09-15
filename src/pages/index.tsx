c86e94f80bff72414b46e2672779d39f211ba3bc
josedvargas31 <dajozavargas@gmail.com>
2025-09-15 11:19:26 -0500

    ⚙️ feat(features): add FeaturesSection and home link; update en/es locales

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Hero from '@/components/Hero'
 import PurposeSection from '@/components/PurposeSection'
+import FeaturesSection from '@/components/FeaturesSection'
 import DefaultLayout from "@/layouts/default";
 
 function index() {
@@ -8,6 +9,7 @@ function index() {
       <DefaultLayout>
         <Hero />
         <PurposeSection />
+        <FeaturesSection />
       </DefaultLayout>
     </>
   )
